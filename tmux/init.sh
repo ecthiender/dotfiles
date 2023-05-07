e18c47216d071fd92a3b34dc65d209010c01bfe1
Anon Ray <rayanon004@gmail.com>
2023-05-07 15:14:07 +0530

    remove dead code in tmux/init.sh

--- tmux/init.sh
+++ tmux/init.sh
@@ -1,52 +1,9 @@
 #!/bin/bash
-# Script to start my tmux sessions. This is the entrypoint to my workflow
-
-# export required env vars
+# Script to start my tmux sessions
 
 # Make sure locale is UTF-8
 locale=$(echo "$LANG" | cut -d '.' -f 1)
 export LC_ALL="$locale.UTF-8"
 export DISABLE_AUTO_TITLE="true"
-# export TERM="xterm-256color"
 
-# load the default sessions I always need
 tmuxp load -2 -y pers work
-
-# Below is the older implementation using tmux directly. This has been then superseded by tmuxp
-#!/bin/bash
-# tmux session initialisation script
-
-# Make sure locale is UTF-8
-# locale=$(echo "$LANG" | cut -d '.' -f 1)
-# export LC_ALL="$locale.UTF-8"
-#
-# tmux="tmux -2"
-#
-# SESSION=$1
-# if [ -z "$SESSION" ]
-# then
-  # SESSION="work"
-# fi
-#
-# workspace1="$server"
-# workspace2="$server"
-# window1=
-#
-# $tmux has-session -t $SESSION > /dev/null
-#
-# if [ $? -eq 0 ]
-# then
-  # echo "Session exists\n"
-  # $tmux attach-session -t $SESSION
-  # exit 0;
-# fi
-#
-# echo "Creating new session $SESSION\n"
-# cd $workspace1
-# $tmux new-session -d -s $SESSION
-# cd $workspace2
-# tmux new-window -t $SESSION:2 -n www-dev
-#
-# $tmux select-window -t $SESSION:1
-# $tmux attach-session -t $SESSION
-# export TERM="xterm-256color"
